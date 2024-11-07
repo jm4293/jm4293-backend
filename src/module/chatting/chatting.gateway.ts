f754c60e52bf5ff85b5e6b18a1be11109db26e82
jm4293 <dlwoals4293@gmail.com>
2024-11-07 17:15:29 +0900

    [Init]: chatting module init

--- src/module/chatting/chatting.gateway.ts
+++ src/module/chatting/chatting.gateway.ts
@@ -3,52 +3,70 @@ import {
   MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
-@WebSocketGateway(4000, {
-  namespace: 'chatting',
-  transports: ['websocket'],
-})
-export class ChattingGateway implements OnGatewayConnection, OnGatewayDisconnect {
+@WebSocketGateway(8081, { namespace: '/socket/chatting', transports: ['websocket'] })
+export class ChattingGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
-  constructor(private readonly configService: ConfigService) {}
+  private readonly logger = new Logger(ChattingGateway.name);
 
-  handleConnection(client: Socket) {
-    console.log('client connected', client.id);
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-    client.on('set-user', (userSeq: string) => {
-      client.data.userSeq = userSeq; // userSeq 저장
-      console.log(`userSeq ${userSeq} has been set for client ${client.id}`);
-    });
+  afterInit(server: Server) {
+    console.log('socket server init', server);
   }
 
-  handleDisconnect(client: Socket) {
+  handleConnection(@ConnectedSocket() client: Socket) {
+    try {
+      console.log('client connected', client.id);
+
+      const accessToken = Object.fromEntries(
+        client.handshake.headers.cookie.split('; ').map((cookie) => cookie.split('=')),
+      )['accessToken'];
+
+      const verifiedAccessToken = this.jwtService.verify(accessToken);
+
+      if (!verifiedAccessToken) {
+        this.logger.error(`${client.id} 연결 강제 종료, status: 401, 토큰이 유효하지 않습니다.`);
+        client.disconnect();
+        return;
+      }
+
+      client.data.userSeq = verifiedAccessToken['userSeq'];
+
+      console.log('client accessToken verifiedAccessToken', verifiedAccessToken);
+
+      // jwt 검증을 통한 인증 작업 로직 시작
+
+      client.join('chatting');
+    } catch (e) {
+      this.logger.error(`${client.id} 연결 강제 종료, status: ${e.status}, ${e.message}`);
+      client.disconnect();
+    }
+  }
+
+  handleDisconnect(@ConnectedSocket() client: Socket) {
     console.log('클라이언트가 연결을 종료했습니다:', client.id);
   }
 
-  // @SubscribeMessage('message')
-  // handleMessage(@MessageBody() message: string, @ConnectedSocket() client: Socket): void {
-  //   console.log('받은 메시지:', message);
-  //   console.log('보낸 사용자 정보:', client.data.user);
-  //   // client.broadcast.emit('message', {
-  //   //   user: client.data.user,
-  //   //   message: message,
-  //   // });
-  //   client.broadcast.emit('message', message);
-  // }
-
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: string, @ConnectedSocket() client: Socket): void {
+  handleMessage(@MessageBody() message: string, @ConnectedSocket() client: Socket) {
     console.log('받은 메시지:', message);
-    console.log('보낸 사용자 정보:', client.data.userSeq); // 사용자 정보를 출력
+    console.log('보낸 사용자 정보:', client.data.userSeq);
 
-    client.broadcast.emit('message', message); // 메시지 전송
+    client.broadcast.emit('message', message);
   }
 }
